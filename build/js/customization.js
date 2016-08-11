1bc82c684b996ab7e685a7d332b78d5093bb2812
Dmitry Kostrubiak <dminked@gmail.com>
2016-08-11 20:01:53 +0300

    Lesson 2.1. Manipulation attributes

--- build/js/customization.js
+++ build/js/customization.js
@@ -1,13 +1,7 @@
 $(document).ready( function() {
 
-    console.log( $( '.block2').html() );
-
-    //$('ul li').html( 'new content' );
-
-    var newContent = $( '<p>This is a new paragraph</p>'),
-        someContent = $( '<p>This is the last paragraph</p>' );
-
-    $( '.par2').html( newContent.text() );
-    $( 'p:last').html( someContent.text() );
+    //$('a').attr( 'target', '_blank' );
+    //$('a').removeAttr( 'href' );
+    $('img').attr( {src : "img/fl-02.jpg", title: "Title"} );
 
 });
