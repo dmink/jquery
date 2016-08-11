1042fc782affa2526eea359eb80763c731ba8613
Dmitry Kostrubiak <dminked@gmail.com>
2016-08-11 11:55:42 +0300

    Lesson 1.3. Content, visibility, child filters

--- build/js/customization.js
+++ build/js/customization.js
@@ -1,10 +1,10 @@
 $(document).ready( function() {
 
-    //$( 'p[class]' ).css('border', '3px solid red');
-    //$( 'p[id=par3]' ).css('border', '3px solid red');
-    //$( 'li[id^=item]' ).css('border', '3px solid red');
-    //$( 'li[id*=item]' ).css('border', '3px solid red');
-    //$( 'li[id$=item]' ).css('border', '3px solid red');
-    $( "p[class*=par][lang^=en]" ).css('border', '3px solid red');
+    //$( "p:contains(3)" ).css('border', '3px solid red');
+    //$( "li:contains(3), p:contains(3)" ).css('border', '3px solid red');
+    //$( "p:nth-child(2n)" ).css('border', '3px solid red');
+    //$( "ul li:last-child" ).css('border', '3px solid red');
+    //$( "p:parent" ).css('border', '3px solid red');
+    $( "ul:has(li[class^=item])" ).css('border', '3px solid red');
 
 });
