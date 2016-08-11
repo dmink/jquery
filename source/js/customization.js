86b2843b07bb8d084618345a384bd8f9fc83f005
Dmitry Kostrubiak <dminked@gmail.com>
2016-08-11 19:19:22 +0300

    Lesson 1.6. Link decorated by file type

--- source/js/customization.js
+++ source/js/customization.js
@@ -1,26 +1,19 @@
 $(document).ready( function() {
 
-/*
-    $( '.empty-top' ).html( 'There are ' +
-        $("li").length + ' elements "li" on the page');
-
-    $( '.empty-bottom' ).text( 'There are ' +
-        $("p").length + ' elements "p" on the page' );
-
-    console.log( $('li').get(1) );
-
-    $('ul').find('li.item3')
-        .css( 'border', '3px solid red' );
-*/
-
-    var leftMargin = 0,
-        border = 3;
-
-    $('p').each( function() {
-        $(this).css( 'border', border + 'px solid red' );
-        $(this).css( 'marginLeft', leftMargin + 'px' );
-        border += 4;
-        leftMargin += 25;
+    $( 'a[href$=pdf]').css({
+        'background-color': 'lightblue'
     });
+    $( 'a[href$=html]').css({
+        'background-color': 'lightcoral'
+    });
+    $( 'a[href$=php]' ).css({
+        'background-color': 'violet'
+    });
+    $( 'a[href*="#"]' ).css({
+        'background-color': 'lightgreen'
+    });
+    $( 'a[href^=mail]' ).css({
+        'background-color': 'yellow'
+    })
 
 });
