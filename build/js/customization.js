5371c809f8651aae8e3a93616424068f3b6c8916
Dmitry Kostrubiak <dminked@gmail.com>
2016-08-14 11:34:59 +0300

    Lesson 2.4. CSS sizing

--- build/js/customization.js
+++ build/js/customization.js
@@ -1,15 +1,24 @@
 $(document).ready( function() {
 
-    $('p:first').wrap('<div class="class"></div>').css({
-        'background-color' : 'yellow'
+    $('.s-height').html( $('.block-size').height() );
+    $('.s-width').html( $('.block-size').width() );
+    $('.s-innerheight').html( $('.block-size').innerHeight() );
+    $('.s-innerwidth').html( $('.block-size').innerWidth() );
+    $('.s-outerheight').html( $('.block-size').outerHeight() );
+    $('.s-outerwidth').html( $('.block-size').outerWidth() );
+    $('.s-offset').html( $('.block-size').offset().top + ' , ' + $('.block-size').offset().left );
+    $('.s-position').html( $('.block-size').position().top + ' , ' + $('.block-size').position().left );
+
+    $('li').addClass('items').removeClass('item1');
+
+    $('.block2 p').click( function() {
+        $(this).toggleClass('.active', 'switch');
     });
 
-    $('p').wrapAll( '<div style="border: 3px solid red"></div>' );
-
-    $('ul li').empty();
-
-    $('.container').wrapInner( '<div style="background-color: #f2f2f2"></div>' );
-
-    $('.block2').clone();
+    $('.scroll-btn').click( function() {
+        var valTop = $('.scroll-wrap').scrollTop();
+        $('.scroll-value').text( 'ScrollTop: ' + valTop );
+        console.log( 'ScrollTop: ' + valTop );
+    });
 
 });
