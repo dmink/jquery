5aef81e5f8c828b09df80d9c64ac79a9fce28364
Dmitry Kostrubiak <dminked@gmail.com>
2016-08-17 14:58:47 +0300

    Lesson 3.4. one()

--- source/js/customization.js
+++ source/js/customization.js
@@ -1,22 +1,11 @@
 $(function() {
 
-    $('.block-data').click( function(evt) {
+    $('.block-data').one( 'click', function(evt) {
 
-        $(this).html('PageX: ' + evt.pageX + ', PageY: ' + evt.pageY + ',<br>' +
-            'Type: ' + evt.type + ',<br>' + 'Target: ' + evt.target);
-
-        if (evt.pageX <= 300) {
-            $(this).css({
-                'background-color': 'tomato'
-            });
-        } else if (evt.pageX > 300 && evt.pageX <= 600) {
-            $(this).css({
-                'background-color': 'lightgreen'
-            });
-        } else {
-            $(this).css({
-                'background-color': 'orange'
-            });
-        };
+        $(this).css({
+            'background-color' : 'lightgreen',
+            'border' : '5px solid darkgreen',
+            'cursor' : 'auto'
+        });
     });
 });
